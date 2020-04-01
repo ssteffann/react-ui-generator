121ad5da1058ed3e6a12a7d6a14dd3170fd66bb5
Stefan Solopa <stefan.solopa@lifeworks.com>
2020-04-01 11:46:25 +0300

    Fixed server file

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs');
+const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -10,11 +11,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'build')));
-  app.get('*', (req, res) => {
-    res.sendfile(path.join(__dirname,
-      'build/index.html'));
-  })
+  app.use(express.static(path.join(__dirname, '../build')));
+
+  app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, '../build', 'index.html'));
+  });
 }
 
 app.get('/api/app-pages', (req, res) => {
